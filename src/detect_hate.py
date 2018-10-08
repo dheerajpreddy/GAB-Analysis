53d47e82ad69540e282e18773db355591dab1a0d
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-10-08 22:52:50 +0530

    Added hates999.json

--- src/detect_hate.py
+++ src/detect_hate.py
@@ -3,7 +3,7 @@ import json
 
 sonar = Sonar()
 
-with open('gabs.json', 'r') as fp:
+with open('gabs999.json', 'r') as fp:
     gabs = json.load(fp)
 fp.close()
 
@@ -12,6 +12,6 @@ responses =[]
 for i in range(len(gabs)):
     responses.append(sonar.ping(text=gabs[i]['post']['body']))
 
-fp2 = open('hates.json', 'w')
+fp2 = open('hates999.json', 'w')
 json.dump(responses, fp2)
 fp2.close()
