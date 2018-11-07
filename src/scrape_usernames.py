90b05fc8be9b7d836d0119c05c3d458e75dfc734
Dheeraj Reddy Pailla <dheerajpreddy@gmail.com>
2018-11-07 18:27:01 +0530

    Added usernames from farmer

--- src/scrape_usernames.py
+++ src/scrape_usernames.py
@@ -9,16 +9,22 @@ queue = ['farmer-general']
 fp = open('data.json', 'w')
 
 while user_count < 50000 and queue:
-	user_name = queue.pop(0)
-	if user_name not in visited:
-		user = gab.getuser(user_name)
-		json.dump(user, fp)
-		user_count += 1
-		print (user_count)
-		visited.add(user_name)
-		followers = gab.getfollowers(user_name, 10000)
-		for user in followers:
-			follower_username = user['username']
-			if follower_username not in visited:
-				queue.append(follower_username)
+	try:
+		user_name = queue.pop(0)
+		if user_name not in visited:
+			user = gab.getuser(user_name)
+			json.dump(user, fp)
+			user_count += 1
+			print (user_count)
+			visited.add(user_name)
+			followers = gab.getfollowers(user_name, 200)
+			for user in followers:
+				try:
+					follower_username = user['username']
+					if follower_username not in visited:
+						queue.append(follower_username)
+				except:
+					continue
+	except:
+		continue
 fp.close()
